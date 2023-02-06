db7b92d87baa1b50838463829484e46e6c52582a
Desale1 <dkdesale08@gmail.com>
2023-02-06 10:07:18 +0530

    create account

--- Account.java
+++ Account.java
@@ -2,36 +2,34 @@ package com.bridgelabz.OOPSExample;
 
 public class Account {
 
+
     private double balance;
-    public Account(){
 
-    }
-    public void deposit(double depositAmount)
+
+    public Account ( double initialBalance )
     {
-        if (depositAmount > 0.0)
-            balance = balance + depositAmount;
-        else
-            System.out.println("Deposit amount is not valid.");
+
+        if ( initialBalance > 0.0 )
+            balance = initialBalance;
     }
-    public void withdraw(double withdrawAmount)
+
+    public void credit( double amount )
     {
-        if (withdrawAmount <= balance)
-            balance = balance - withdrawAmount;
-        else
-            System.out.println("Withdrawal amount exceeded account balance.");
+        balance = balance + amount;
     }
+    public void debit( double debitAmount )
+    {
 
-    public double getBalance() {
 
+        if (debitAmount > balance)
+        {
+            debitAmount = 0.0;
+            System.out.println( "Debit amount exceeded account balance." );
+        }
+        balance = balance - debitAmount;
+    }
+    public double getBalance()
+    {
         return balance;
-
     }
-
-    public static void main(String[] args) {
-        Account a = new Account();
-        a.deposit(1000);
-        a.withdraw(5000);
-        System.out.println(a.balance);
-    }
-
 }
